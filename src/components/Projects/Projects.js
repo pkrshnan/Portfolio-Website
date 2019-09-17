92a64185c823d107b8b6f742e88cac8315588303
Pranav Krishnan <pranavkrsh@gmail.com>
2019-09-17 14:20:16 -0400

    Finishing projects (except images)

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import { Section, Container, Heading, Card, Columns } from 'react-bulma-components'
+import { Section, Container, Heading, Card, Columns, Image } from 'react-bulma-components'
+
+// CSS
 import './style.css'
+
+// Images
+// TODO: Add the two images for RemindMe! and BudgetBuddy
+import personalWebsite from '../../images/personal_website.png'
+import aggregate from '../../images/aggregate.png'
 
 export default class Projects extends React.Component {
   render() {
@@ -8,46 +15,66 @@ export default class Projects extends React.Component {
       <Section id="projects" className="background-skyline">
         <Container>
           <Heading className="section-heading">Projects</Heading>
-          <Card className="project-card-left">
-            <Card.Content>
-              <Heading size={5}>Personal Website</Heading>
-              <ul className="project-subpoints">
-                <li>Amet quia deleniti doloremque commodi id, incidunt, magni psam praesentium dignissimos labore repellat. Est earum explicabo?</li>
-                <li>Lorem accusamus velit libero non labore illo facilis possiccusamus fugit vitae voluptates ipsum dolorum Tempora unde aliquid!</li>
-                <li>Dolor ipsum labore a velit excepturi Voluptate aliquam quadit sed consectetur delectus officia. Et explicabo ab nesciunt?</li>
-              </ul>
-            </Card.Content>
-          </Card>
-          <Card className="project-card-right">
-            <Card.Content>
-              <Heading size={5}>Aggregate</Heading>
-              <ul className="project-subpoints">
-                <li>Amet quia deleniti doloremque commodi id, incidunt, magni psam praesentium dignissimos labore repellat. Est earum explicabo?</li>
-                <li>Lorem accusamus velit libero non labore illo facilis possiccusamus fugit vitae voluptates ipsum dolorum Tempora unde aliquid!</li>
-                <li>Dolor ipsum labore a velit excepturi Voluptate aliquam quadit sed consectetur delectus officia. Et explicabo ab nesciunt?</li>
-              </ul>
-            </Card.Content>
-          </Card>
-          <Card className="project-card-left">
-            <Card.Content>
-              <Heading size={5}>RemindMe!</Heading>
-              <ul className="project-subpoints">
-                <li>Amet quia deleniti doloremque commodi id, incidunt, magni psam praesentium dignissimos labore repellat. Est earum explicabo?</li>
-                <li>Lorem accusamus velit libero non labore illo facilis possiccusamus fugit vitae voluptates ipsum dolorum Tempora unde aliquid!</li>
-                <li>Dolor ipsum labore a velit excepturi Voluptate aliquam quadit sed consectetur delectus officia. Et explicabo ab nesciunt?</li>
-              </ul>
-            </Card.Content>
-          </Card>
-          <Card className="project-card-right">
-            <Card.Content>
-              <Heading size={5}>BudgetBuddy</Heading>
-              <ul className="project-subpoints">
-                <li>Amet quia deleniti doloremque commodi id, incidunt, magni psam praesentium dignissimos labore repellat. Est earum explicabo?</li>
-                <li>Lorem accusamus velit libero non labore illo facilis possiccusamus fugit vitae voluptates ipsum dolorum Tempora unde aliquid!</li>
-                <li>Dolor ipsum labore a velit excepturi Voluptate aliquam quadit sed consectetur delectus officia. Et explicabo ab nesciunt?</li>
-              </ul>
-            </Card.Content>
-          </Card>
+          <Columns>
+            <Columns.Column size="one-half">
+              <a href="https://github.com/pkrshnan/personal-website" target="_blank">
+                <Card className="project-card">
+                  <Image src={personalWebsite} />
+                  <Card.Content>
+                    <Heading size={5}>Personal Website</Heading>
+                    <p className="project-stack">React</p>
+                    <p className="project-subpoints">
+                      A single-page web portfolio built using Bulma CSS and React.
+                    </p>
+                  </Card.Content>
+                </Card>
+              </a>
+            </Columns.Column>
+            <Columns.Column size="one-half">
+              <a href="https://github.com/pkrshnan/aggregate" target="_blank">
+                <Card className="project-card">
+                  <Image src={aggregate} />
+                  <Card.Content>
+                    <Heading size={5}>Aggregate</Heading>
+                    <p className="project-stack">Vue, Heroku</p>
+                    <p className="project-subpoints">
+                      A news aggregator built using Vue and hosted on Heroku, collecting the latest articles from Google News, Hacker News and Reddit.
+                    </p>
+                  </Card.Content>
+                </Card>
+              </a>
+            </Columns.Column>
+          </Columns>
+          <Columns>
+            <Columns.Column size="one-half">
+              <a href="https://github.com/pkrshnan/remindme" target="_blank">
+                <Card className="project-card">
+                  <Image src={personalWebsite} />
+                  <Card.Content>
+                    <Heading size={5}>RemindMe!</Heading>
+                    <p className="project-stack">Java, Android Studio</p>
+                    <p className="project-subpoints">
+                      A reminders application that allows you to set a location for each reminder, and subsequently notifies you when you arrive at that location.
+                    </p>
+                  </Card.Content>
+                </Card>
+              </a>
+            </Columns.Column>
+            <Columns.Column size="one-half">
+              <a href="https://github.com/pkrshnan/budgetbuddy-java" target="_blank">
+                <Card className="project-card">
+                  <Image src={personalWebsite} />
+                  <Card.Content>
+                    <Heading size={5}>BudgetBuddy</Heading>
+                    <p className="project-stack">React Native, Java</p>
+                    <ul className="project-subpoints">
+                      A proactive budgeting application that personalizes a budget for you based on prior spending habits. Created at a hackathon and won a $1000 prize.
+                    </ul>
+                  </Card.Content>
+                </Card>
+              </a>
+            </Columns.Column>
+          </Columns>
         </Container>
       </Section>
     )
